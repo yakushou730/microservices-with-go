5511e62595338a0f0f8dc2a2af681189396dad31
Shou Tseng <yakushou730@gmail.com>
2020-02-22 15:26:39 +0800

    2-2-4-4

--- section-2-2-4-1/main.go
+++ section-2-2-4-1/main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.Handle("/example", HeaderDecorator(http.HandlerFunc(myHandlerFunc)))
-	http.Handle("/example2", HeaderDecorator(http.HandlerFunc(myHandlerFunc2)))
+	logger := log.New(os.Stdout, "Server: ", log.Lshortfile)
+	logDecorator := logDecoratorCreator(logger)
+
+	http.Handle("/example", logDecorator(HeaderDecorator(http.HandlerFunc(myHandlerFunc))))
+	http.Handle("/example2", logDecorator(HeaderDecorator(http.HandlerFunc(myHandlerFunc2))))
 
 	log.Println("Starting Server")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -18,14 +22,14 @@ func myHandlerFunc(w http.ResponseWriter, r *http.Request) {
 	// Write Status Code
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/text")
-	fmt.Fprintln(w, "Example-1; Request was succesful")
+	fmt.Fprintln(w, "Example-1; Request was successful")
 }
 
 func myHandlerFunc2(w http.ResponseWriter, r *http.Request) {
 	// Write Status Code
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/text")
-	fmt.Fprintln(w, "Example-2; Request was succesful")
+	fmt.Fprintln(w, "Example-2; Request was successful")
 }
 
 func HeaderDecorator(h http.Handler) http.Handler {
@@ -35,9 +39,14 @@ func HeaderDecorator(h http.Handler) http.Handler {
 	})
 }
 
-func logDecorator(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf(r.RequestURI+": We have a connection from %s", r.RemoteAddr)
-		h.ServeHTTP(w, r)
-	})
+type HttpDecorator func(http.Handler) http.Handler
+
+func logDecoratorCreator(logger *log.Logger) HttpDecorator {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			logger.Printf(r.RequestURI+": We have a connection from %s", r.RemoteAddr)
+			defer logger.Println("Connection Closed")
+			h.ServeHTTP(w, r)
+		})
+	}
 }
